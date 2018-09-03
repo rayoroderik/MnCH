28587641e4a9381cb0a0db954c9adbf0ea8af1fe
Anisa Budiarthati <anisabudiarthati@10-60-48-168.0200.bsd.id.iosda.org>
2018-09-03 22:12:56 +0700

    tableview staff side

--- MnCH/Controller/staffMainViewController.swift
+++ MnCH/Controller/staffMainViewController.swift
@@ -8,10 +8,26 @@
 
 import UIKit
 
-class staffMainViewController: UIViewController {
+class staffMainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var kaderTableView: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! staffMainTableViewCell
+        
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        kaderTableView.delegate = self
+        kaderTableView.dataSource = self
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {

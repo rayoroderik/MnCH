31e877b4e1c4011f608b4611e861fdbab4bebd1f
Anisa Budiarthati <anisabudiarthati@10-60-48-168.0200.bsd.id.iosda.org>
2018-09-03 19:19:57 +0700

    kadermain tableview

--- MnCH/Controller/kaderMainViewController.swift
+++ MnCH/Controller/kaderMainViewController.swift
@@ -8,11 +8,25 @@
 
 import UIKit
 
-class kaderMainViewController: UIViewController {
+class kaderMainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var kaderMainTableView: UITableView!
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! kaderMainTableViewCell
+        cell.lblName.text = "Nama Anak \(indexPath.row)"
+        
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        kaderMainTableView.delegate = self
+        kaderMainTableView.dataSource = self
         // Do any additional setup after loading the view.
     }
 

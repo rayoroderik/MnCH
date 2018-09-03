320c5fbb61980979cdb1d2508191b14d1768cf2a
Anisa Budiarthati <anisabudiarthati@10-60-48-168.0200.bsd.id.iosda.org>
2018-09-03 20:10:56 +0700

    detailKader

---
+++ MnCH/Controller/detailKaderTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  detailKaderTableViewCell.swift
+//  MnCH
+//
+//  Created by Anisa Budiarthati on 03/09/18.
+//  Copyright © 2018 Rayo Roderik. All rights reserved.
+//
+
+import UIKit
+
+class detailKaderTableViewCell: UITableViewCell {
+    @IBOutlet weak var childImg: UIImageView!
+    @IBOutlet weak var lblName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

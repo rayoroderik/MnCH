d168b7935346564ae721c7776e55ebe4d9d033b8
Steven Muliamin <steven.muliamin@gmail.com>
2018-09-04 13:47:35 +0700

    added globalKader

---
+++ MnCH/GlobalKader.swift
@@ -0,0 +1,14 @@
+//
+//  GlobalKader.swift
+//  MnCH
+//
+//  Created by Steven Muliamin on 04/09/18.
+//  Copyright © 2018 Rayo Roderik. All rights reserved.
+//
+
+import Foundation
+
+struct GlobalKader {
+    static var loginState: Bool = false
+    static var kader: KaderModel!
+}

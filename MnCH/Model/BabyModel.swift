d00e9444138618ccb607ffca96e81a23a05dd33e
Steven Muliamin <steven.muliamin@gmail.com>
2018-09-04 10:47:50 +0700

    added IDs to models

--- MnCH/Model/BabyModel.swift
+++ MnCH/Model/BabyModel.swift
@@ -11,18 +11,23 @@ import Foundation
 
 class BabyModel {
     var babyName: String
+    var babyAddress: String
     var dob: Date
     var dobString: String
     var gender: String
     var momName: String
     var momPhone: String
+    var babyID: String
     
-    init(babyName: String, dob: Date, dobString: String, gender: String, momName: String, momPhone: String) {
+    init(babyName: String, babyAddress: String,dob: Date, dobString: String, gender: String, momName: String, momPhone: String, babyID: String) {
         self.babyName = babyName
+        self.babyAddress = babyAddress
         self.dob = dob
         self.dobString = dobString
         self.gender = gender
         self.momName = momName
         self.momPhone = momPhone
+        
+        self.babyID = babyID
     }
 }

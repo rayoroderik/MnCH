c8c138485676de98782b2256bd145ee91ff2e41f
Marco <febrianomarco@gmail.com>
2018-09-04 15:53:36 +0700

    addhistory

--- MnCH/Controller/addHistoryViewController.swift
+++ MnCH/Controller/addHistoryViewController.swift
@@ -10,12 +10,24 @@ import UIKit
 
 class addHistoryViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+    var babyID: String = ""
+    
+    
+    @IBOutlet weak var beratBadanTF: UITextField!
+    @IBOutlet weak var tinggiBadanTF: UITextField!
+    @IBOutlet weak var descTV: UITextView!
+    
     @IBOutlet weak var imagePicked: UIImageView!
     let imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         imagePicker.delegate = self
+        let date = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd-MM-yyyy"
+        let dateString = dateFormatter.string(from: date)
+        print(dateString)
     }
     
     @IBAction func imagePickerClicked(_ sender: Any) {
@@ -33,5 +45,68 @@ class addHistoryViewController: UIViewController, UIImagePickerControllerDelegat
         dismiss(animated: true, completion: nil)
     }
     
+    
+    @IBAction func updateHistoryChildAction(_ sender: Any) {
+        
+        self.register()
+        
+//        FirebaseReferences.databaseRef.observeSingleEvent(of: .value) { (snap) in
+//            if snap.hasChildren() == false {
+//                self.register()
+//            }
+//            else {
+//                FirebaseReferences.databaseRef.child("babies").observeSingleEvent(of: .value, with: { (snap) in
+//                    let tempBabies = snap.value as! [String:Any]
+//
+//                    // ambil list of kaders dari 1 place
+//                    for (key, _) in tempBabies{
+//                        let tempSingleBabies = tempBabies[key] as! [String:Any]
+//                        let babyCheck: [String:String] = tempSingleBabies["babyCheck"] as! [String:String]
+//
+//                        // ambil single kader dari list of kaders
+//                        for (key, _) in babyCheck{
+//                            let tempSingleBabyCheck = babyCheck[key] as! [String:Any]
+//
+//                            let babyCheckID: String = tempSingleBabyCheck["babyCheckID"] as! String
+//
+//                            if
+//
+//
+//
+//                        }
+//                    }
+//                    self.register()
+//                })
+//            }
+//        }
+    }
+    
+    
+    func register(){
+        
+        //getDate
+        let date = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd-MM-yyyy"
+        let dateString = dateFormatter.string(from: date)
+        print(dateString)
+        
+        let tempBeratBadan = self.beratBadanTF.text
+        let tempTinggiBadan = self.tinggiBadanTF.text
+        let tempKeterangan = self.descTV.text
+        let babyCheckID = UUID().uuidString
+        
+        var tempBabiesCheck: [String:Any] = [:]
+        
+        tempBabiesCheck["weight"] = tempBeratBadan
+        tempBabiesCheck["height"] = tempTinggiBadan
+        tempBabiesCheck["notes"] = tempKeterangan
+        tempBabiesCheck["date"] = dateString
+        
+        FirebaseReferences.databaseRef.child("babies/testBabyID/babyCheck/\(babyCheckID)").setValue("")
+        FirebaseReferences.databaseRef.child("babyCheck/\(babyCheckID)").setValue(tempBabiesCheck)
+        
+        print("addHistoryBaby")
+    }
 
 }

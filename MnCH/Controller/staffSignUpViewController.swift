f0be5817a3c6e205f14e9c668cec7f5c7d3640c2
Anisa Budiarthati <anisabudiarthati@10-60-48-168.0200.bsd.id.iosda.org>
2018-09-04 15:51:56 +0700

    staff sign up done

--- MnCH/Controller/staffSignUpViewController.swift
+++ MnCH/Controller/staffSignUpViewController.swift
@@ -22,6 +22,84 @@ class staffSignUpViewController: UIViewController, UIPickerViewDelegate {
         super.viewDidLoad()
         addPickerArea()
     }
+    @IBAction func signUpClicked(_ sender: Any) {
+        
+        if (self.nameTextField.text == "" || self.phoneTextField.text == "" || self.passTextField.text == "" || self.verifyPassTextField.text == "" ||
+            self.areaPicker.text == ""){
+            
+            let alert = UIAlertController(title: "Pendaftaran Gagal", message: "Mohon isi semua field!", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default) { (UIAlertAction) in
+                return
+            }
+            alert.addAction(okAction)
+            self.present(alert, animated: true, completion: nil)
+            return
+        }
+        else if (self.passTextField.text != self.verifyPassTextField.text){
+            let alert = UIAlertController(title: "Pendaftaran Gagal", message: "Kata sandi tidak sesuai!", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default) { (UIAlertAction) in
+                return
+            }
+            alert.addAction(okAction)
+            self.present(alert, animated: true, completion: nil)
+            return
+        }
+        
+        FirebaseReferences.databaseRef.child("staffs").observeSingleEvent(of: .value) { (snap) in
+            if (snap == nil || snap.hasChildren() == false){
+                // belom ada yg kedaftar
+                self.register()
+            }
+            else{
+                // udh ada yg daftar
+                FirebaseReferences.databaseRef.child("staffs").observeSingleEvent(of: .value, with: { (snap) in
+                    let staffIDs = snap.value as! [String:Any]
+                    
+                    // ambil list of staffs
+                    for (key, _) in staffIDs{
+                        let tempSingleStaff = staffIDs[key] as! [String:Any]
+                        let staffPhone: String = tempSingleStaff["staffPhone"] as! String
+                        if (staffPhone == self.phoneTextField.text){
+                            // udah ada yg pernah pake phonenya
+                            print("udah ada")
+                            
+                            let alert = UIAlertController(title: "Pendaftaran Gagal", message: "Nomor telfon sudah pernah terdaftar!", preferredStyle: .alert)
+                            let okAction = UIAlertAction(title: "OK", style: .default) { (UIAlertAction) in
+                                return
+                            }
+                            alert.addAction(okAction)
+                            self.present(alert, animated: true, completion: nil)
+                            return
+                        }
+                    }
+                    // aman
+                    self.register()
+                })
+            }
+    }
+    }
+    
+    
+    func register(){
+        
+        let uniqueID = UUID().uuidString
+        
+        var tempStaff: [String:Any] = [:]
+        
+        tempStaff["staffName"] = self.nameTextField.text
+        tempStaff["staffPhone"] = self.phoneTextField.text
+        tempStaff["staffArea"] = self.areaPicker.text
+        
+        FirebaseReferences.databaseRef.child("staffs/\(uniqueID)").setValue(tempStaff)
+        
+        let alert = UIAlertController(title: "Pendaftaran Berhasil", message: "Anda telah mendaftar di ____!", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { (UIAlertAction) in
+            return
+        }
+        alert.addAction(okAction)
+        self.present(alert, animated: true, completion: nil)
+        print("registered")
+    }
 }
 
 extension staffSignUpViewController: UIPickerViewDataSource {

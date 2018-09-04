6c94c4cb773ed4a596f84ff9d318620374ab4042
Rayo Roderik <rayoroderik@gmail.com>
2018-09-04 14:52:36 +0700

    edit addBaby

--- MnCH/Controller/addChildViewController.swift
+++ MnCH/Controller/addChildViewController.swift
@@ -8,28 +8,71 @@
 
 import UIKit
 
-class addChildViewController: UIViewController {
+class addChildViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var pickBabyImage: UIImageView!
+    @IBOutlet weak var babyNameTF: UITextField!
+    @IBOutlet weak var jenisKelaminTF: UITextField!
+    @IBOutlet weak var momNameTF: UITextField!
+    @IBOutlet weak var momPhoneTF: UITextField!
+    @IBOutlet weak var babyAddressTF: UITextView!
+    @IBOutlet weak var hariLahirTF: UITextField!
+    @IBOutlet weak var bulanLahirTF: UITextField!
+    @IBOutlet weak var tahunLahirTF: UITextField!
+    let pickImage = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGestureRecognizer:)))
+        pickBabyImage.isUserInteractionEnabled = true
+        pickBabyImage.addGestureRecognizer(tapGestureRecognizer)
+        pickImage.delegate = self
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            pickBabyImage.contentMode = .scaleAspectFit
+            pickBabyImage.image = selectedImage
+        }
+        dismiss(animated: true, completion: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @objc func imageTapped(tapGestureRecognizer: UITapGestureRecognizer)
+    {
+        let tappedImage = tapGestureRecognizer.view as! UIImageView
+        
+        // Your action
+        
+        pickImage.allowsEditing = false
+        pickImage.sourceType = .photoLibrary
+        
+        present(pickImage, animated: true, completion: nil)
     }
-    */
-
+    
+    var kaderModel : KaderModel?
+    
+    func addNewBaby(){
+        let babyID = UUID().uuidString
+        
+        var tempBaby: [String:Any] = [:]
+        
+        
+        
+        tempBaby["babyName"] = self.babyNameTF
+        tempBaby["babyAddress"] = self.babyAddressTF.text
+        tempBaby["dobString"] = "\(self.hariLahirTF.text)/\(self.bulanLahirTF.text)/\(self.tahunLahirTF.text)"
+        tempBaby["gender"] = self.jenisKelaminTF.text
+        tempBaby["momName"] = self.momNameTF.text
+        tempBaby["momPhone"] = self.momPhoneTF.text
+        
+        
+        
+        FirebaseReferences.databaseRef.child("kaders/pedongkelan/\(hehe)/bayi/\(babyID)").setValue(tempBaby)
+        
+        print("registered")
+    }
+    
 }
